0a68bcd052e4ac663b03577b725f8ae0c9b68fd1
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2023-04-07 17:52:22 +0200

    Init for Rest Api

---
+++ fleetManagerRestApi/Fleet_Manager_Rest_Api/Fleet_Manager_Rest_Api/src/main/java/com/bfiejdasz/fleet_manager_rest_api/FleetManagerRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.bfiejdasz.fleet_manager_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagerRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetManagerRestApiApplication.class, args);
+    }
+
+}
---
+++ fleetManagerRestApi/Fleet_Manager_Rest_Api/Fleet_Manager_Rest_Api/src/test/java/com/bfiejdasz/fleet_manager_rest_api/FleetManagerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfiejdasz.fleet_manager_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagerRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
