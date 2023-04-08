8d96214fecd2743e92c5b29d07db904f66856e00
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2023-04-08 17:13:29 +0200

    Added planed repairs controller

---
+++ fleetManagerRestApi/Fleet_Manager_Rest_Api/Fleet_Manager_Rest_Api/src/main/java/com/bfiejdasz/fleet_manager_rest_api/controller/PlanedRepairsController.java
@@ -0,0 +1,2 @@
+package com.bfiejdasz.fleet_manager_rest_api.controller;public class PlanedRepairsController {
+}
---
+++ fleetManagerRestApi/Fleet_Manager_Rest_Api/Fleet_Manager_Rest_Api/src/main/java/com/bfiejdasz/fleet_manager_rest_api/repository/IPlanedRepairsRepository.java
@@ -0,0 +1,2 @@
+package com.bfiejdasz.fleet_manager_rest_api.repository;public interface IPlanedRepairsRepository {
+}
