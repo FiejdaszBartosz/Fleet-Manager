33d0223861bfa11a30f367ee11479b78f2360bac
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2023-05-08 09:27:06 +0200

    Added VehiclesController

---
+++ Fleet_Manager_Android_App/app/src/main/java/com/bfiejdasz/fleet_manager_android_app/api/api_controllers/VehiclesController.java
@@ -0,0 +1,48 @@
+package com.bfiejdasz.fleet_manager_android_app.api.api_controllers;
+
+import com.bfiejdasz.fleet_manager_android_app.api.ApiClient;
+import com.bfiejdasz.fleet_manager_android_app.api.api_interfaces.IApiVehicles;
+import com.bfiejdasz.fleet_manager_android_app.api.entity.VehiclesEntity;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class VehiclesController {
+    private final IApiVehicles apiVehicles;
+
+    public VehiclesController() {
+        apiVehicles = ApiClient.getClient().create(IApiVehicles.class);
+    }
+
+    public void getVehicles(Callback<List<VehiclesEntity>> callback) {
+        Call<List<VehiclesEntity>> call = apiVehicles.getVehicles();
+        BaseController<List<VehiclesEntity>> baseController = new BaseController<>(call);
+        baseController.execute(callback);
+    }
+
+    public void getVehiclesById(int id, Callback<VehiclesEntity> callback) {
+        Call<VehiclesEntity> call = apiVehicles.getVehiclesById(id);
+        BaseController<VehiclesEntity> baseController = new BaseController<>(call);
+        baseController.execute(callback);
+    }
+
+    public void deleteVehiclesById(int id, Callback<Void> callback) {
+        Call<Void> call = apiVehicles.deleteVehiclesById(id);
+        BaseController<Void> baseController = new BaseController<>(call);
+        baseController.execute(callback);
+    }
+
+    public void createUser(VehiclesEntity vehicle, Callback<VehiclesEntity> callback) {
+        Call<VehiclesEntity> call = apiVehicles.createVehicles(vehicle);
+        BaseController<VehiclesEntity> baseController = new BaseController<>(call);
+        baseController.execute(callback);
+    }
+
+    public void updateVehiclesById(int id, VehiclesEntity vehicle, final Callback<VehiclesEntity> callback) {
+        Call<VehiclesEntity> call = apiVehicles.updateVehiclesById(id, vehicle);
+        BaseController<VehiclesEntity> baseController = new BaseController<>(call);
+        baseController.execute(callback);
+    }
+}
---
+++ Fleet_Manager_Android_App/app/src/main/java/com/bfiejdasz/fleet_manager_android_app/api/api_interfaces/IApiVehicles.java
@@ -0,0 +1,30 @@
+package com.bfiejdasz.fleet_manager_android_app.api.api_interfaces;
+
+import com.bfiejdasz.fleet_manager_android_app.api.entity.VehiclesEntity;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface IApiVehicles {
+    @GET("vehicles/getAll")
+    Call<List<VehiclesEntity>> getVehicles();
+
+    @GET("vehicles")
+    Call<VehiclesEntity> getVehiclesById(@Query("id") int id);
+
+    @DELETE("vehicles")
+    Call<Void> deleteVehiclesById(@Query("id") int id);
+
+    @POST("vehicles")
+    Call<VehiclesEntity> createVehicles(@Body VehiclesEntity vehicle);
+
+    @PATCH("vehicles")
+    Call<VehiclesEntity> updateVehiclesById(@Query("id") int id, @Body VehiclesEntity vehicle);
+}
